c74dfff24c8ef9a7eb46ca9e3730f7c32ad6c87f
ZhongYu <zhongyu.vip@gmail.com>
2021-01-24 12:29:44 +0800

    初始化项目

---
+++ src/app/shared/shared-delon.module.ts
@@ -0,0 +1,6 @@
+import { PageHeaderModule } from '@delon/abc/page-header';
+import { SEModule } from '@delon/abc/se';
+import { STModule } from '@delon/abc/st';
+import { SVModule } from '@delon/abc/sv';
+
+export const SHARED_DELON_MODULES = [PageHeaderModule, STModule, SVModule, SEModule];
---
+++ src/app/shared/shared-zorro.module.ts
@@ -0,0 +1,7 @@
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+export const SHARED_ZORRO_MODULES = [NzButtonModule, NzGridModule, NzTableModule, NzFormModule, NzInputModule];
